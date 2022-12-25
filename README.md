0f6559455fd6cb78e836ab2ed7bafc0055b7061f
yuxiangwei0808 <2234607879@qq.com>
2022-12-25 16:36:58 +0800

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # AI-based_circuit_design
+
+The repo for the final-year project. 
+
+## Usage
+[`read_netlist.py`] (/read_netlist.py) read the original LTSpice netlist files and obtains components list and graph with Networkx. [`helpers.py`] (/helpers.py) contains necessary utilities. [`find_trees.py`] (/find_trees.py) returns all spanning trees. 
---
+++ find_trees.py
@@ -0,0 +1,58 @@
+def _expand(G, explored_nodes, explored_edges):
+    """
+    Expand existing solution by a process akin to BFS.
+
+    Arguments:
+    ----------
+    G: networkx.Graph() instance
+        full graph
+
+    explored_nodes: set of ints
+        nodes visited
+
+    explored_edges: set of 2-tuples
+        edges visited
+
+    Returns:
+    --------
+    solutions: list, where each entry in turns contains two sets corresponding to explored_nodes and explored_edges
+        all possible expansions of explored_nodes and explored_edges
+
+    """
+    frontier_nodes = list()
+    frontier_edges = list()
+    for v in explored_nodes:
+        for u in nx.neighbors(G,v):
+            if not (u in explored_nodes):
+                frontier_nodes.append(u)
+                frontier_edges.append([(u,v), (v,u)])
+
+    return zip([explored_nodes | frozenset([v]) for v in frontier_nodes], [explored_edges | frozenset(e) for e in frontier_edges])
+
+def find_all_spanning_trees(G, root=0):
+    """
+    Find all spanning trees of a Graph.
+
+    Arguments:
+    ----------
+    G: networkx.Graph() instance
+        full graph
+
+    Returns:
+    ST: list of networkx.Graph() instances
+        list of all spanning trees
+
+    """
+
+    # initialise solution
+    explored_nodes = frozenset([root])
+    explored_edges = frozenset([])
+    solutions = [(explored_nodes, explored_edges)]
+    # we need to expand solutions number_of_nodes-1 times
+    for ii in range(G.number_of_nodes()-1):
+        # get all new solutions
+        solutions = [_expand(G, nodes, edges) for (nodes, edges) in solutions]
+        # flatten nested structure and get unique expansions
+        solutions = set([item for sublist in solutions for item in sublist])
+
+    return [nx.from_edgelist(edges) for (nodes, edges) in solutions]
