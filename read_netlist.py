48db9f0740416a1ca8c0d5a8d017bbb81e10dd48
yuxiangwei0808 <2234607879@qq.com>
2023-01-10 17:37:46 +0800

    TODO: Replace nx.Graph with lcapy.Circuit

--- read_netlist.py
+++ read_netlist.py
@@ -8,7 +8,7 @@ from find_trees import find_all_spanning_trees
 
 path = "./data/ltspice_examples/"
 files = [path+f for f in listdir(path) if isfile(join(path, f))]
-files = ['./raw_netlist/figure30.net']
+# files = ['./raw_netlist/figure26.net']
 netlists = ((f, open(f, 'rb').read().decode('utf-8', 'ignore')) for f in files)
 
 valid_files = [(f, src) for (f, src) in netlists if h.is_valid_netlist(src)]
@@ -18,7 +18,7 @@ for (f, src) in valid_files:
         # print(f)
         component_list, g = h.netlist_to_graph(src)
         t = time.time()
-        tree = g.find_single_tree()
+        tree = [x for x in g.find_single_tree()]
         print(time.time() - t) # 94
         two_tree = g.find_two_tree(tree, [[1, 6], [4, 2]])
 #         graph_data[index] = (component_list, g)
